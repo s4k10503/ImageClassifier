db1e167f9a777a14c519bf01df581fc55580a2e4
0y0 <s4k1@outlook.jp>
2023-12-04 00:51:41 +0900

    apply mvp pattern
    - Introduced RxPY and Injector to classify roles into model, presenter, and view.

--- src/gui.py
+++ src/gui.py
@@ -1,32 +1,34 @@
 import tkinter as tk
 import customtkinter as ctk
+from PIL import Image, ImageTk
+from rx.subject.subject import Subject
 
 FONT_TYPE = "Arial"
 
 
-class ImageClassifierApp(ctk.CTk):
+class ImageClassifierView(ctk.CTk):
     """
-    A GUI application for image classification using CustomTkinter.
-
-    Args:
-        select_image_callback (callable): Callback function to handle image selection.
-
-    Attributes:
-        button_select (customtkinter.CTkButton): Button to select an image.
-        label_image (customtkinter.CTkLabel): Label to display the selected image.
-        label_result (customtkinter.CTkLabel): Label to display the predicted label.
-        label_confidence (customtkinter.CTkLabel): Label to display the confidence level.
+    A custom Tkinter class for creating the GUI of an image classifier app.
+    This class manages the layout and UI elements for image classification.
     """
 
-    def __init__(self, select_image_callback):
+    def __init__(self):
         """
-        Initialize the ImageClassifierApp.
-
-        Args:
-            select_image_callback (callable): Callback function to handle image selection.
+        Initializes the ImageClassifierView object.
+        Sets up the initial UI components and state.
         """
         super().__init__()
+        self.tk_image = None
+        self.image_selected_subject = Subject()
+        self.setup_ui()
 
+    def setup_ui(self):
+        """
+        Set up the user interface for the ImageClassifierView.
+
+        This includes setting the appearance mode, color theme, form size,
+        and initializing the main frame and its widgets.
+        """
         # Set CustomTkinter form design settings
         ctk.set_appearance_mode("dark")
         ctk.set_default_color_theme("blue")
@@ -39,18 +41,65 @@ class ImageClassifierApp(ctk.CTk):
         main_frame = ctk.CTkFrame(self)
         main_frame.pack(expand=True, fill=tk.BOTH)
 
-        # Display the "Select Image" button
-        self.button_select = ctk.CTkButton(main_frame, text="Select Image", command=select_image_callback, font=(FONT_TYPE, 20))
+        self.create_widgets(main_frame)
+
+    def create_widgets(self, main_frame):
+        """
+        Create and add widgets to the main frame.
+
+        Args:
+            main_frame: The main frame in which widgets are placed.
+        """
+        # "Select Image" button
+        self.button_select = ctk.CTkButton(
+            main_frame, text="Select Image",
+            command=self.on_select_image, font=(FONT_TYPE, 20))
         self.button_select.pack(pady=10)
 
-        # Display the image label
-        self.label_image = ctk.CTkLabel(main_frame, text="", width=100, height=100)
+        # Image label
+        self.label_image = ctk.CTkLabel(
+            main_frame, text="", width=300, height=300)
         self.label_image.pack(pady=10)
 
-        # Display the predicted label
-        self.label_result = ctk.CTkLabel(main_frame, text="Predicted Label: ", font=(FONT_TYPE, 20))
+        # Predicted label
+        self.label_result = ctk.CTkLabel(
+            main_frame, text="Predicted Label: ", font=(FONT_TYPE, 20))
         self.label_result.pack(pady=5)
 
-        # Display the confidence level
-        self.label_confidence = ctk.CTkLabel(main_frame, text="Confidence: ", font=(FONT_TYPE, 20))
+        # Confidence level
+        self.label_confidence = ctk.CTkLabel(
+            main_frame, text="Confidence: ", font=(FONT_TYPE, 20))
         self.label_confidence.pack(pady=5)
+
+    def on_select_image(self):
+        """
+        Handle the event when the 'Select Image' button is clicked.
+
+        Currently, it triggers an event in the image_selected_subject.
+        """
+        self.image_selected_subject.on_next(None)
+
+    def display_image(self, image_path):
+        """
+        Display an image on the UI.
+
+        Args:
+            image_path: The path of the image to be displayed.
+        """
+        image = Image.open(image_path)
+        max_thumbnail_size = (256, 256)
+        image.thumbnail(max_thumbnail_size)
+        tk_image = ImageTk.PhotoImage(image)
+        self.label_image.configure(image=tk_image)
+        self.tk_image = tk_image  # Keep a reference
+
+    def display_result(self, result, confidence):
+        """
+        Display the classification result and confidence on the UI.
+
+        Args:
+            result: The classification result.
+            confidence: The confidence level of the classification.
+        """
+        self.label_result.configure(text="Predicted Label: " + result)
+        self.label_confidence.configure(text=f"Confidence: {confidence:.2f}%")
--- src/main.py
+++ src/main.py
@@ -1,54 +1,99 @@
-from gui import ImageClassifierApp
-from inference import perform_inference, load_labels, initialize_onnx_model
-
-from PIL import Image, ImageTk
 from tkinter import filedialog
-import warnings
+from injector import Injector, inject, singleton
+from gui import ImageClassifierView
+from inference import ImageClassifierModel
 
 
-def select_image():
+class ImageClassifierPresenter:
     """
-    Callback function to select an image using a file dialog.
+    Presenter class in the MVC architecture for an image classifier app.
 
-    Args:
-        None
-
-    Returns:
-        None
+    It handles the interactions between the view and the model.
     """
-    global tk_image
-    file_path = filedialog.askopenfilename()
-    image = Image.open(file_path)
 
-    max_thumbnail_size = (256, 256)
-    image.thumbnail(max_thumbnail_size)
-    tk_image = ImageTk.PhotoImage(image)
-    app.label_image.configure(image=tk_image)
+    @inject
+    def __init__(self, view: ImageClassifierView, model: ImageClassifierModel):
+        """
+        Initialize the presenter.
 
-    predicted_label, confidence = perform_inference(file_path, sess, input_name, class_labels)
-    app.label_result.configure(text="Predicted Label: " + predicted_label)
-    app.label_confidence.configure(text=f"Confidence: {confidence:.2f}%")
+        Args:
+            view (ImageClassifierView): The view part of the MVP.
+            model (ImageClassifierModel): The model part of the MVP.
+        """
+        self.view = view
+        self.model = model
+        view.image_selected_subject.subscribe(
+            on_next=lambda _: self.on_image_selected()
+        )
+
+    def setup_model(self):
+        """
+        Set up the image classification model with the necessary files.
+        """
+        model_path = "../model/resnet18-v1-7.onnx"
+        labels_path = "../json/imagenet_class_index.json"
+        self.model.load_model(model_path, labels_path)
+
+    def start(self):
+        """
+        Start the GUI event loop.
+        """
+        self.view.mainloop()
+
+    def on_image_selected(self):
+        """
+        Handle the image selection event.
+
+        Opens a file dialog for the user to select an image and
+        then displays and classifies the selected image.
+        """
+        file_path = filedialog.askopenfilename()
+        if file_path:
+            self.view.display_image(file_path)
+            self.classify_image(file_path)
+
+    def classify_image(self, image_path):
+        """
+        Perform image classification on the selected image.
+
+        Args:
+            image_path (str): The path to the selected image.
+        """
+        predicted_label, confidence = self.model.perform_inference(image_path)
+        self.view.display_result(predicted_label, confidence)
+
+
+class Dependency:
+    """
+    A class for managing dependencies in the application using the Injector.
+
+    It configures how different components of the application are instantiated and managed.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialize the Dependency class with an Injector.
+        """
+        self.injector = Injector(self.configure)
+
+    @staticmethod
+    def configure(binder):
+        """
+        Configure the binder for dependency injection.
+
+        Args:
+            binder: The binder instance used in dependency injection.
+        """
+        binder.bind(ImageClassifierView,
+                    to=ImageClassifierView, scope=singleton)
+        binder.bind(ImageClassifierModel,
+                    to=ImageClassifierModel, scope=singleton)
+        binder.bind(ImageClassifierPresenter,
+                    to=ImageClassifierPresenter, scope=singleton)
 
 
 if __name__ == "__main__":
-
-    warnings.simplefilter('ignore')
-
-    # Path to the JSON file containing class labels for the model
-    json_path = '../json/imagenet_class_index.json'
-
-    # Path to the ONNX model file
-    model_path = '../model/resnet18-v1-7.onnx'
-
-    # Load class labels from JSON file
-    class_labels = load_labels(json_path)
-
-    # Initialize the ONNX model session and get the input name
-    sess, input_name = initialize_onnx_model(model_path)
-    tk_image = None
-
-    # Create an instance of the ImageClassifierApp class
-    app = ImageClassifierApp(select_image)
-
-    # Start the main event loop of the GUI application
-    app.mainloop()
+    dependency = Dependency()
+    presenter = dependency.injector.get(ImageClassifierPresenter)
+    presenter.setup_model()
+    presenter.start()
